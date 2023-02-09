a3edd8f40a363514f6461c0215a115f656400838
felixpradoh <felixpradoh@gmail.com>
2023-02-09 19:36:31 +0100

    update

---
+++ Data Wrangling, Analysis and AB Testing with SQL/Week_3/Product Analysis (Practical Exercise).sql
@@ -0,0 +1,107 @@
+-- Exercise 0: ​Count how many users we have 
+
+SELECT COUNT(*)
+FROM dsv1069.users
+LIMIT 100
+
+
+-- Exercise 1: ​Find out how many users have ever ordered 
+
+SELECT
+    COUNT(DISTINCT user_id)  AS users_with_orders
+FROM
+    dsv1069.orders
+
+-- Exercise 2: Goal find how many users have reordered the same item 
+
+SELECT
+    COUNT(DISTINCT user_id) AS users_who_reordered
+FROM
+    (
+    SELECT 
+        user_id,
+        item_id,
+        COUNT(DISTINCT line_item_id) AS times_user_ordered
+    FROM dsv1069.orders
+    GROUP BY
+        user_id,
+        item_id
+    ) AS user_level_orders
+WHERE times_user_ordered > 1
+
+
+-- Exercise 3: Do users even order more than once? 
+
+SELECT
+    COUNT(DISTINCT user_id)
+FROM
+    (
+    SELECT 
+        user_id,
+        COUNT(DISTINCT invoice_id) AS order_count
+    FROM dsv1069.orders
+    GROUP BY user_id
+    ) AS user_level
+WHERE order_count > 1
+
+
+-- Exercise 4: Orders per item 
+
+SELECT
+    item_id,
+    COUNT(line_item_id) AS times_ordered
+FROM dsv1069.orders
+
+
+-- Exercise 5: Orders per category 
+
+SELECT * FROM dsv1069.orders
+
+-- Exercise 6: Goal: Do user order multiple things from the same category? 
+
+SELECT
+    user_id,
+    item_id,
+    COUNT(line_item_id) AS item_ordered
+FROM 
+    dsv1069.orders
+GROUP BY
+    user_id,
+    item_id
+
+
+-- Exercise 7: Goal: Find the average time between orders. Decide if this analysis is necessary 
+
+SELECT
+    first_orders.user_id,
+    DATE(first_orders.paid_at)                               AS first_order_date,
+    DATE(second_orders.paid_at)                              AS secon_order_date,
+    DATE(second_orders.paid_at) - DATE(first_orders.paid_at) AS date_diff
+FROM
+    (
+    SELECT
+        user_id,
+        invoice_id,
+        paid_at,
+        DENSE_RANK( ) OVER (PARTITION BY user_id ORDER BY paid_at ASC)
+            AS order_num
+    FROM
+        dsv1069.orders
+    ) AS first_orders
+JOIN
+    (
+    SELECT
+        user_id,
+        invoice_id,
+        paid_at,
+        DENSE_RANK( ) OVER (PARTITION BY user_id ORDER BY paid_at ASC)
+            AS order_num
+    FROM
+        dsv1069.orders
+    ) AS second_orders
+ON 
+    first_orders.user_id = second_orders.user_id
+WHERE
+    first_orders.order_num = 1
+AND
+    second_orders.order_num = 2
--- Data Wrangling, Analysis and AB Testing with SQL/Week_3/Promo Email (Practice Exercise).sql
+++ Data Wrangling, Analysis and AB Testing with SQL/Week_3/Promo Email (Practice Exercise).sql
@@ -1,6 +1,9 @@
 -- Exercise 1: Create the right subtable for recently viewed events using the view_item_events table.
 
-SELECT *
+SELECT user_id,
+       item_id,
+       event_time,
+       ROW_NUMBER ( ) OVER (PARTITION BY user_id ORDER BY event_time DESC) AS view_number,
 FROM dsv1069.view_item_events
 
 -- Exercise 2: Check your joins. Join your tables together recent_views, users, items
@@ -26,7 +29,58 @@ JOIN dsv1069.items
 --             outline from the outline in EX2 and pull only the columns you need. Make sure they are named
 --             appropriately so that another human can read and understand their contents.
 
+SELECT users.id             AS user_id,
+       users.email_address,
+       items.id             AS item_id,
+       items.name           AS item_name,
+       items.category       AS item_category
+FROM
+    (
+    SELECT user_id,
+           item_id,
+           event_time,
+           ROW_NUMBER( ) OVER (PARTITION BY user_id ORDER BY event_time DESC) AS view_number
+    FROM
+        dsv1069.view_item_events
+    ) AS recent_views
+
+JOIN dsv1069.users
+    ON   users.id = recent_views.user_id
+JOIN dsv1069.items
+    ON   items.id = recent_views.item_id
+
 -- Exercise 4: Consider any edge cases. If we sent an email to everyone in the results of this
 --             query, what would we want to filter out. Add in any extra filtering that you think would make this
 --             email better. For example should we include deleted users? Should we send this email to users
 --             who already ordered the item they viewed most recently?
+
+SELECT 
+    COALESCE(users.parent_user_id, users.id)     AS user_id,
+       users.email_address,
+       items.id                                  AS item_id,
+       items.name                                AS item_name,
+       items.category                            AS item_category
+FROM
+    (
+    SELECT user_id,
+           item_id,
+           event_time,
+           ROW_NUMBER( ) OVER (PARTITION BY user_id ORDER BY event_time DESC) AS view_number
+    FROM
+        dsv1069.view_item_events
+    WHERE
+        event_time > '2017-01-01'
+    ) AS recent_views
+
+JOIN dsv1069.users
+    ON   users.id = recent_views.user_id
+LEFT OUTER JOIN dsv1069.orders
+    ON   orders.item_id = recent_views.item_id
+    AND orders.user_id = recent_views.user_id
+WHERE 
+    view_number = 1
+AND
+    users.deleted_at IS NOT NULL
+AND 
+    orders.item_id IS NULL
+3
