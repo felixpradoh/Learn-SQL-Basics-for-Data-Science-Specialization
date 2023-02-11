d2e05cfb852102a5da0376646e9b1e2362e99393
felixpradoh <felixpradoh@gmail.com>
2023-02-11 20:25:19 +0100

    update week 4

--- Data Wrangling, Analysis and AB Testing with SQL/Week_4/Analyzing Results (Practice Exercise).sql
+++ Data Wrangling, Analysis and AB Testing with SQL/Week_4/Analyzing Results (Practice Exercise).sql
@@ -1,102 +1,147 @@
--- Exercise 1: Using the table from Exercise 4.3 and compute a metric that measures
---             Whether a user created an order after their test assignment
-  
-  --Requirements: Even if a user had zero orders, we should have a row that counts
-  --              their number of orders as zero. If the user is not in the experiment
-  --              they should not be included.
-  
-  SELECT
-    test_events.test_id,
-    test_events.test_assignment,
-    test_events.user_id,
-    MAX(CASE WHEN orders.created_at > test_events.event_time 
-             THEN 1 
-             ELSE 0
-        END) AS orders_after_assignment_binary
-  FROM
-  (
-  SELECT
-    event_id,
-    event_time,
-    user_id,
+-- Exercise 1: Use the order_binary metric from the previous exercise, count the number of users
+--             per treatment group for test_id = 7, and count the number of users with orders (for test_id 7)
 
-    MAX(CASE WHEN parameter_name = 'test_id'
-             THEN CAST(parameter_value As int)
-             ELSE NULL
-             END) AS test_id,
-    MAX(CASE WHEN parameter_name = 'test_assignment'
-             THEN CAST(parameter_value As int)
-             ELSE NULL
-             END) AS test_assignment
-  FROM
-    dsv1069.events
-  WHERE
-    event_name  = 'test_assignment'
-  GROUP BY
-    event_id,
-    event_time,
-    user_id
-  ) AS test_events
-  LEFT JOIN
-    dsv1069.orders
-  ON
-    orders.user_id = test_events.user_id
-  GROUP BY
-    test_events.test_id,
-    test_events.test_assignment,
-    test_events.user_id
-  
+SELECT test_assignment,
+       COUNT(user_id) AS users,
+       SUM(order_binary) AS users_with_orders
+FROM
+  (SELECT assignments.user_id,
+          assignments.test_id,
+          assignments.test_assignment,
+          MAX(CASE WHEN orders.created_at > assignments.event_time THEN 1 ELSE 0 END) AS order_binary
+   FROM
+     (SELECT event_id,
+             event_time,
+             user_id,
+             MAX(CASE WHEN parameter_name = 'test_id' THEN CAST(parameter_value AS int) ELSE NULL END) AS test_id,
+             MAX(CASE WHEN parameter_name = 'test_assignment' THEN CAST(parameter_value AS int) ELSE NULL END) AS test_assignment
+      FROM dsv1069.events
+      GROUP BY event_id,
+               event_time,
+               user_id
+      ORDER BY event_id) AS assignments
+   LEFT OUTER JOIN dsv1069.orders
+     ON assignments.user_id = orders.user_id
+   GROUP BY assignments.user_id,
+            assignments.test_id,
+            assignments.test_assignment) AS user_level
+WHERE test_id = 7
+GROUP BY test_assignment;
 
--- Exercise 2:
-  --Using the table from the previous exercise, add the following metrics
-  --1) the number of orders/invoices
-  --2) the number of items/line-items ordered
-  --3) the total revenue from the order after treatment
-SELECT
-    test_events.test_id,
-    test_events.test_assignment,
-    test_events.user_id,
-    COUNT(DISTINCT (CASE WHEN orders.created_at > test_events.event_time
-                         THEN invoice_id
-                         ELSE NULL
-                         END)) AS orders_after_assignment,
-    COUNT(DISTINCT (CASE WHEN orders.created_at > test_events.event_time
-                     THEN line_item_id
-                     ELSE NULL
-                     END)) AS items_after_assignment,
-    SUM(CASE WHEN orders.created_at > test_events.event_time 
-             THEN price 
-             ELSE 0 
-             END) AS total_revenue
-    FROM
-      (
-        SELECT
-        event_id,
-        event_time,
-        user_id,
-        MAX(CASE WHEN parameter_name = 'test_id'
-                 THEN CAST(parameter_value As int)
-                 ELSE NULL
-                 END) AS test_id,
-        MAX(CASE WHEN parameter_name = 'test_assignment'
-                 THEN CAST(parameter_value As int)
-                 ELSE NULL
-                 END) AS test_assignment
-  FROM
-    dsv1069.events
-  WHERE
-    event_name  = 'test_assignment'
-  GROUP BY
-    event_id,
-    event_time,
-    user_id
-  ) AS test_events
-  LEFT JOIN
-    dsv1069.orders
-  ON
-    orders.user_id = test_events.user_id
-  GROUP BY
-    test_events.test_id,
-    test_events.test_assignment,
-    test_events.user_id
-    
+
+-- Exercise 2: Create a new tem view binary metric. Count the number of users per treatment
+--             group, and count the number of users with views (for test_id 7)
+
+SELECT test_assignment,
+       COUNT(user_id) AS users,
+       SUM(views_binary) AS views_binary
+FROM
+  (SELECT assignments.user_id,
+          assignments.test_id,
+          assignments.test_assignment,
+          MAX(CASE WHEN views.event_time > assignments.event_time THEN 1 ELSE 0 END) AS views_binary
+   FROM
+     (SELECT event_id,
+             event_time,
+             user_id,
+             MAX(CASE WHEN parameter_name = 'test_id' THEN CAST(parameter_value AS int) ELSE NULL END) AS test_id,
+             MAX(CASE WHEN parameter_name = 'test_assignment' THEN CAST(parameter_value AS int) ELSE NULL END) AS test_assignment
+      FROM dsv1069.events
+      GROUP BY event_id,
+               event_time,
+               user_id
+      ORDER BY event_id) AS assignments
+   LEFT OUTER JOIN
+     (SELECT *
+      FROM dsv1069.events
+      WHERE event_name = 'view_item' ) AS views
+   ON assignments.user_id = views.user_id
+   GROUP BY assignments.user_id,
+            assignments.test_id,
+            assignments.test_assignment) AS user_level
+WHERE test_id = 7
+GROUP BY test_assignment;
+
+-- Exercise 3: Alter the result from EX 2, to compute the users who viewed an item WITHIN 30
+--             days of their treatment event
+
+SELECT test_assignment,
+       COUNT(user_id) AS users,
+       SUM(views_binary) AS views_binary,
+       SUM(views_binary_30d) AS views_binary_30d
+FROM
+  (SELECT assignments.user_id,
+          assignments.test_id,
+          assignments.test_assignment,
+          MAX(CASE WHEN views.event_time > assignments.event_time THEN 1 ELSE 0 END) AS views_binary,
+          MAX(CASE WHEN (views.event_time > assignments.event_time AND DATE_PART('day', views.event_time - assignments.event_time) <= 30) THEN 1 ELSE 0
+              END) AS views_binary_30d
+   FROM
+     (SELECT event_id,
+             event_time,
+             user_id,
+             MAX(CASE WHEN parameter_name = 'test_id' THEN CAST(parameter_value AS int) ELSE NULL END) AS test_id,
+             MAX(CASE WHEN parameter_name = 'test_assignment' THEN CAST(parameter_value AS int) ELSE NULL END) AS test_assignment
+      FROM dsv1069.events
+      GROUP BY event_id,
+               event_time,
+               user_id
+      ORDER BY event_id) AS assignments
+   LEFT OUTER JOIN
+     (SELECT *
+      FROM dsv1069.events
+      WHERE event_name = 'view_item' ) views
+     ON assignments.user_id = views.user_id
+   GROUP BY assignments.user_id,
+            assignments.test_id,
+            assignments.test_assignment) AS user_level
+WHERE test_id = 7
+GROUP BY test_assignment;
+
+-- Exercise 4:
+-- Create the metric invoices (this is a mean metric, not a binary metric) and for test_id = 7
+-- The count of users per treatment group
+-- The average value of the metric per treatment group
+-- The standard deviation of the metric per treatment group
+
+SELECT test_id,
+       test_assignment,
+       COUNT(user_id)         AS users,
+       AVG(invoices)          AS avg_invoices,
+       STDDEV(invoices)       AS stddev_invoices
+FROM
+  (SELECT assignments.user_id,
+          assignments.test_id,
+          assignments.test_assignment,
+          COUNT(DISTINCT
+                CASE WHEN orders.created_at > assignments.event_time THEN orders.invoice_id ELSE NULL
+                END) AS invoices,
+          COUNT(DISTINCT 
+                CASE WHEN orders.created_at > assignments.event_time THEN orders.line_item_id ELSE NULL
+                END) AS line_items,
+          COALESCE(SUM(
+                CASE WHEN orders.created_at > assignments.event_time THEN orders.price ELSE 0
+                END), 0) AS total_revenue
+   FROM
+     (SELECT event_id,
+             event_time,
+             user_id,
+             MAX(CASE WHEN parameter_name = 'test_id' THEN CAST(parameter_value AS int)
+                      ELSE NULL
+                      END) AS test_id,
+             MAX(CASE WHEN parameter_name = 'test_assignment' THEN CAST(parameter_value AS int)
+                      ELSE NULL
+                      END) AS test_assignment
+      FROM dsv1069.events
+      GROUP BY event_id,
+               event_time,
+               user_id
+      ORDER BY event_id) AS assignments
+   LEFT OUTER JOIN dsv1069.orders
+   ON assignments.user_id = orders.user_id
+   GROUP BY assignments.user_id,
+            assignments.test_id,
+            assignments.test_assignment) AS mean_metrics
+GROUP BY test_id,
+         test_assignment
+ORDER BY test_id
